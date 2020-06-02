19a3c5660e49d8504cd4824553a507f850e0ab5f
preich <preich@192.168.1.101>
2020-06-02 23:17:20 +0200

    firefox and chrome

--- src/main/java/HelloSelenium.java
+++ src/main/java/HelloSelenium.java
@@ -8,10 +8,9 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.common.io.Files;
@@ -22,20 +21,23 @@ import com.google.common.io.Files;
  */
 public class HelloSelenium {
 
-	public static void run() throws IOException {
-        WebDriver driver = new FirefoxDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        try {
-            driver.get("https://google.com/ncr");
-            driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
-            WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3>div")));
-            System.out.println(firstResult.getAttribute("textContent"));
-            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-         // Now you can do whatever you need to do with it, for example copy somewhere
-            System.out.println(scrFile);
-            Files.copy(scrFile, new File("1.png"));
-        } finally {
-            driver.quit();
-        }
-    }
+	public static void run(WebDriver driver) throws IOException {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		try {
+			driver.get("https://google.com/ncr");
+			driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+			WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3>div")));
+			System.out.println(firstResult.getAttribute("textContent"));
+			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			System.out.println(scrFile);
+			File dstDir = new File("screenshots");
+			dstDir.mkdirs();
+			File dstFile = new File(dstDir,
+					driver.getClass().getSimpleName() + "_" + System.currentTimeMillis() + ".png");
+			Files.copy(scrFile, dstFile);
+			System.out.println(dstFile);
+		} finally {
+			driver.quit();
+		}
+	}
 }
--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -1,9 +1,16 @@
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloSeleniumTest {
 
 	@Test
-	public void test() throws Exception {
-		HelloSelenium.run();
+	public void testFirefox() throws Exception {
+		HelloSelenium.run(new FirefoxDriver());
+	}
+
+	@Test
+	public void testChrome() throws Exception {
+		HelloSelenium.run(new ChromeDriver());
 	}
 }
