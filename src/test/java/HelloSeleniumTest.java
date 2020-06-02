228228bd1d9ee551848930eeb6168448b2e6940c
preich <preich@192.168.1.101>
2020-06-03 00:43:30 +0200

    chrome still doesn't work

--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -1,6 +1,8 @@
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloSeleniumTest {
@@ -13,6 +15,18 @@ public class HelloSeleniumTest {
 //	@Ignore//TODO: fix chrome
 	@Test
 	public void testChrome() throws Exception {
-		HelloSelenium.run(new ChromeDriver());
+		ChromeOptions options = new ChromeOptions();
+		
+		System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
+//        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); //
+        options.addArguments("--headless"); //
+		ChromeDriverService service = ChromeDriverService.createDefaultService();
+		ChromeDriver driver = new ChromeDriver(service, options);
+		HelloSelenium.run(driver);
 	}
 }
